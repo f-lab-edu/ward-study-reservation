22e3662bc04cae34109d76b97ba38d083792e8f1
seonggon do <62453668+mooh2jj@users.noreply.github.com>
2022-04-22 03:09:35 +0900

    api url 메서드에 옮김

--- src/main/java/com/dsg/wardstudy/controller/studyGroup/StudyGroupController.java
+++ src/main/java/com/dsg/wardstudy/controller/studyGroup/StudyGroupController.java
@@ -14,41 +14,48 @@ import java.util.List;
 @Slf4j
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/study-group")
 public class StudyGroupController {
 
     private final StudyGroupService studyGroupService;
 
-    @PostMapping
+    @PostMapping("/study-group")
     public ResponseEntity<StudyGroupResponse> create(@RequestBody StudyGroupRequest studyGroupRequest) {
         log.info("studyGroup create");
         return new ResponseEntity<>(studyGroupService.create(studyGroupRequest), HttpStatus.CREATED);
     }
 
-    @GetMapping("{id}")
-    public ResponseEntity<StudyGroupResponse> getById(@PathVariable("id") Long groupId) {
+    @GetMapping("/study-group/{id}")
+    public ResponseEntity<StudyGroupResponse> getById(@PathVariable("id") Long studyGroupId) {
         log.info("studyGroup getById");
-        return ResponseEntity.ok(studyGroupService.getById(groupId));
+        return ResponseEntity.ok(studyGroupService.getById(studyGroupId));
     }
 
-    @GetMapping
+    @GetMapping("/study-group")
     public ResponseEntity<List<StudyGroupResponse>> getAll() {
         log.info("studyGroup getAll");
         return ResponseEntity.ok(studyGroupService.getAll());
     }
 
-    @PutMapping("{id}")
+//    @GetMapping("/user/{id}/study-group/")
+//    public ResponseEntity<List<StudyGroupResponse>> getAllByUserId(
+//            @PathVariable("id") Long userId
+//    ) {
+//        log.info("studyGroup getAllByUserId");
+//        return ResponseEntity.ok(studyGroupService.getAllByUserId(userId));
+//    }
+
+    @PutMapping("/study-group/{id}")
     public Long updateById(
-            @PathVariable("id") Long groupId,
+            @PathVariable("id") Long studyGroupId,
             @RequestBody StudyGroupRequest studyGroupRequest) {
         log.info("studyGroup updateById");
-        return studyGroupService.updateById(groupId, studyGroupRequest);
+        return studyGroupService.updateById(studyGroupId, studyGroupRequest);
     }
 
-    @DeleteMapping("{id}")
-    public ResponseEntity<String> deleteById(@PathVariable("id") Long groupId) {
+    @DeleteMapping("/study-group/{id}")
+    public ResponseEntity<String> deleteById(@PathVariable("id") Long studyGroupId) {
         log.info("studyGroup deleteById");
-        studyGroupService.deleteById(groupId);
+        studyGroupService.deleteById(studyGroupId);
         return new ResponseEntity<>("a study-group successfully deleted!", HttpStatus.OK);
     }
 }
