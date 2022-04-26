393d74637a21fc4554bb4e0cfcdf87df315613bf
seonggon do <62453668+mooh2jj@users.noreply.github.com>
2022-04-26 15:06:47 +0900

    findByRoomIdAndTime indentation 정리

--- src/main/java/com/dsg/wardstudy/repository/reservation/ReservationRepository.java
+++ src/main/java/com/dsg/wardstudy/repository/reservation/ReservationRepository.java
@@ -20,8 +20,8 @@ public interface ReservationRepository extends JpaRepository<Reservation, Long> 
     @Query("select r from Reservation r left join fetch r.room where r.room.id = :roomId " +
             "and r.startTime > :sTime and r.endTime < :eTime")
     List<Reservation> findByRoomIdAndTime(@Param("roomId") String roomId,
-                                              @Param("sTime") LocalDateTime sTime,
-                                              @Param("eTime") LocalDateTime eTime);
+                                          @Param("sTime") LocalDateTime sTime,
+                                          @Param("eTime") LocalDateTime eTime);
 
     @Query("select r from Reservation r left join fetch r.room where r.room.id = :roomId " +
             "and r.id = :reservationId")
