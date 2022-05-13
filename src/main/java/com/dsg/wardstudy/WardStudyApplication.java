a810a3709d962d93f8cb0796877f381a64e3edb3
seonggon do <62453668+mooh2jj@users.noreply.github.com>
2022-05-14 02:16:18 +0900

    batch first setting

--- src/main/java/com/dsg/wardstudy/WardStudyApplication.java
+++ src/main/java/com/dsg/wardstudy/WardStudyApplication.java
@@ -1,9 +1,11 @@
 package com.dsg.wardstudy;
 
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableBatchProcessing
 @SpringBootApplication
 public class WardStudyApplication {
 
---
+++ src/main/java/com/dsg/wardstudy/config/batch/SimpleJobConfig.java
@@ -0,0 +1,38 @@
+package com.dsg.wardstudy.config.batch;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.repeat.RepeatStatus;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Slf4j
+@RequiredArgsConstructor
+@Configuration
+public class SimpleJobConfig {
+
+    private final JobBuilderFactory jobBuilderFactory;
+    private final StepBuilderFactory stepBuilderFactory;
+
+    @Bean
+    public Job simpleJob() {
+        return jobBuilderFactory.get("simpleJob")
+                .start(simpleStep1())
+                .build();
+    }
+
+    @Bean
+    public Step simpleStep1() {
+        return stepBuilderFactory.get("simpleStep1")
+                .tasklet((contribution, chunkContext) -> {
+                    log.info(">>>>> This is Step1");
+                    return RepeatStatus.FINISHED;
+                })
+                .build();
+    }
+
+}
