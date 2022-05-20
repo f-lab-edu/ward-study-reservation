11fa8b1359301d752a0b71ea54902c3d15ba0a78
seonggon do <62453668+mooh2jj@users.noreply.github.com>
2022-05-20 16:41:01 +0900

    BatchJobScheduler 구현 - 스케쥴링 기능 추가 30분 마다 알림메일 배치

---
+++ src/main/java/com/dsg/wardstudy/config/batch/BatchJobScheduler.java
@@ -0,0 +1,36 @@
+package com.dsg.wardstudy.config.batch;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.JobParameter;
+import org.springframework.batch.core.JobParameters;
+import org.springframework.batch.core.launch.JobLauncher;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Slf4j
+@Component
+@RequiredArgsConstructor
+public class BatchJobScheduler {
+
+    private final JobLauncher jobLauncher;
+
+    private final Job notificationAlarmJob;
+
+    @Scheduled(cron = "0 0/30 * * * *")
+    public void runJob() {
+
+        Map<String, JobParameter> parameters = new HashMap<>();
+        parameters.put("timestamp", new JobParameter(System.currentTimeMillis()));
+
+        try {
+            jobLauncher.run(notificationAlarmJob, new JobParameters(parameters));
+        } catch (Exception e){
+            log.error(e.getMessage());
+        }
+    }
+}
