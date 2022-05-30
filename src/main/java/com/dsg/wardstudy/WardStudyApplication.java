b58c4bb478e08bde9084e12c3f00dfb900427841
seonggon do <62453668+mooh2jj@users.noreply.github.com>
2022-05-30 17:56:07 +0900

    Redis 설정 - Application @EnableCaching 추가

--- src/main/java/com/dsg/wardstudy/WardStudyApplication.java
+++ src/main/java/com/dsg/wardstudy/WardStudyApplication.java
@@ -3,12 +3,13 @@ package com.dsg.wardstudy;
 import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @EnableBatchProcessing
 @SpringBootApplication
 @EnableScheduling
+@EnableCaching
 public class WardStudyApplication {
 
     public static void main(String[] args) {
