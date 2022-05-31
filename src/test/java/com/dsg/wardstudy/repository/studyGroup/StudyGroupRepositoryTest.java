ab5fc68cddc04f3fa4792cfca48079d6a7f0068d
seonggon do <62453668+mooh2jj@users.noreply.github.com>
2022-05-31 23:41:16 +0900

    StudyGroupRepositoryTest 수정

--- src/test/java/com/dsg/wardstudy/repository/studyGroup/StudyGroupRepositoryTest.java
+++ src/test/java/com/dsg/wardstudy/repository/studyGroup/StudyGroupRepositoryTest.java
@@ -1,25 +1,28 @@
 package com.dsg.wardstudy.repository.studyGroup;
 
 import com.dsg.wardstudy.domain.studyGroup.StudyGroup;
+import com.dsg.wardstudy.domain.user.User;
 import com.dsg.wardstudy.domain.user.UserGroup;
 import com.dsg.wardstudy.repository.user.UserGroupRepository;
-import com.dsg.wardstudy.repository.user.UserRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.ActiveProfiles;
 
 import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
+import java.util.stream.LongStream;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 @Slf4j
 @DataJpaTest
+@ActiveProfiles("test")
 class StudyGroupRepositoryTest {
 
     @Autowired
@@ -28,10 +31,10 @@ class StudyGroupRepositoryTest {
     @Autowired
     private UserGroupRepository userGroupRepository;
 
-    @Autowired
-    private UserRepository userRepository;
-
     private StudyGroup studyGroup;
+    private User user;
+    private UserGroup userGroup;
+
 
     @BeforeEach
     void setup() {
@@ -39,6 +42,17 @@ class StudyGroupRepositoryTest {
                 .id(1L)
                 .title("testSG")
                 .content("인원 4명의 스터디그룹을 모집합니다.")
+                .build();
+
+        user = User.builder()
+                .id(1L)
+                .name("dsg")
+                .email("dsg@gmail.com")
+                .build();
+
+        userGroup = UserGroup.builder()
+                .user(user)
+                .studyGroup(studyGroup)
                 .build();
     }
 
@@ -84,11 +98,11 @@ class StudyGroupRepositoryTest {
         // given - precondition or setup
         StudyGroup savedStudyGroup = studyGroupRepository.save(studyGroup);
         // when - action or the behaviour that we are going test
-        studyGroupRepository.findById(savedStudyGroup.getId());
+        StudyGroup findStudyGroup = studyGroupRepository.findById(savedStudyGroup.getId()).get();
 
         // then - verify the output
         assertThat(this.studyGroup).isNotNull();
-        assertThat(studyGroup.getTitle()).isEqualTo("testSG");
+        assertThat(findStudyGroup.getTitle()).isEqualTo("testSG");
 
     }
 
@@ -97,14 +111,18 @@ class StudyGroupRepositoryTest {
     public void givenUserId_whenFindByUserId_thenReturnStudyGroupsIdList() {
         // given - precondition or setup
         // TODO : User, UserGroup save API 만들어야
+        userGroupRepository.save(userGroup);
+
         // when - action or the behaviour that we are going test
-        List<UserGroup> iByUserId = userGroupRepository.findByUserId(2L);
-        List<Long> studyGroupsIds = iByUserId.stream()
-                .map(d -> d.getStudyGroup().getId())
+        List<UserGroup> userGroupList = userGroupRepository.findByUserId(user.getId());
+        log.info("userGroupList: {}", userGroupList);
+        List<Long> studyGroupsIds = userGroupList.stream()
+                .map(ug -> ug.getStudyGroup().getId())
                 .collect(Collectors.toList());
         log.info("studyGroupsIds : {}", studyGroupsIds);
 
         assertThat(studyGroupsIds).isNotNull();
+        assertThat(studyGroupsIds).isEqualTo(List.of(studyGroup.getId()));
 
     }
 
@@ -112,6 +130,15 @@ class StudyGroupRepositoryTest {
     @DisplayName("스터디그룹들 studyGroupIdList로 가져오기")
     public void givenSGIdList_whenFindBySGIdIn_thenStudyGroupList(){
         // given - precondition or setup
+        LongStream.rangeClosed(1, 3).forEach(i -> {
+            StudyGroup studyGroup = StudyGroup.builder()
+                    .id(i)
+                    .title("testSG_" + i)
+                    .content("인원 " + i + "명의 스터디그룹을 모집합니다.")
+                    .build();
+            studyGroupRepository.save(studyGroup);
+        });
+
         List<Long> studyGroupsIds = List.of(1L, 2L, 3L);
 
         // when - action or the behaviour that we are going test
@@ -120,6 +147,7 @@ class StudyGroupRepositoryTest {
         // then - verify the output
         assertThat(studyGroups).isNotNull();
         log.info("studyGroups.size(): " + studyGroups.size());
+        assertThat(studyGroups.size()).isEqualTo(3);
 
     }
 
