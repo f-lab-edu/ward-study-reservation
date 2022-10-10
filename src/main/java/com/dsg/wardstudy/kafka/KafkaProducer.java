575b4464918c3d54dfb89ed1ccbaf2c3632173d8
seonggon do <62453668+mooh2jj@users.noreply.github.com>
2022-10-10 18:27:30 +0900

    kafka class 구현

---
+++ src/main/java/com/dsg/wardstudy/kafka/KafkaProducer.java
@@ -0,0 +1,21 @@
+package com.dsg.wardstudy.kafka;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Service;
+
+@Service
+public class KafkaProducer {
+    private static final String TOPIC = "exam";
+    private final KafkaTemplate<String, String> kafkaTemplate;
+
+    @Autowired
+    public KafkaProducer(KafkaTemplate kafkaTemplate) {
+        this.kafkaTemplate = kafkaTemplate;
+    }
+
+    public void sendMessage(String message) {
+        System.out.println(String.format("Produce message : %s", message));
+        this.kafkaTemplate.send(TOPIC, message);
+    }
+}
