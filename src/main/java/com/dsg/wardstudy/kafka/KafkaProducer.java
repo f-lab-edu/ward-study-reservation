8409c2fdf19c4b1bdde8bbd6797f0e0c65ca6ace
seonggon do <62453668+mooh2jj@users.noreply.github.com>
2022-10-11 17:20:29 +0900

    [#33] JsonKafka 구현

--- src/main/java/com/dsg/wardstudy/kafka/KafkaProducer.java
+++ src/main/java/com/dsg/wardstudy/kafka/KafkaProducer.java
@@ -1,18 +1,15 @@
 package com.dsg.wardstudy.kafka;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.RequiredArgsConstructor;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 public class KafkaProducer {
     private static final String TOPIC = "exam";
     private final KafkaTemplate<String, String> kafkaTemplate;
 
-    @Autowired
-    public KafkaProducer(KafkaTemplate kafkaTemplate) {
-        this.kafkaTemplate = kafkaTemplate;
-    }
 
     public void sendMessage(String message) {
         System.out.println(String.format("Produce message : %s", message));
