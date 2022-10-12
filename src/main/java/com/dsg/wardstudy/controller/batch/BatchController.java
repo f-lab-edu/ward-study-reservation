aa6bb079fe2fd682e32eb5a562accd2988260c88
seonggon do <62453668+mooh2jj@users.noreply.github.com>
2022-10-12 16:18:25 +0900

    Update BatchController.java

--- src/main/java/com/dsg/wardstudy/controller/batch/BatchController.java
+++ src/main/java/com/dsg/wardstudy/controller/batch/BatchController.java
@@ -13,9 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.HashMap;
 import java.util.Map;
 
-/**
- * test1011
- */
+
 @RestController
 @RequestMapping("/batch")
 @RequiredArgsConstructor
