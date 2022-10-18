6cb9f503f22157832123b68f70523f072ef4e12b
seonggon do <62453668+mooh2jj@users.noreply.github.com>
2022-10-18 21:49:50 +0900

    [#42] kafka publish -> consume 이메일 알림 구현

--- src/main/java/com/dsg/wardstudy/common/adapter/kafka/JsonKafkaConsumer.java
+++ src/main/java/com/dsg/wardstudy/common/adapter/kafka/JsonKafkaConsumer.java
@@ -1,9 +1,11 @@
 package com.dsg.wardstudy.common.adapter.kafka;
 
-import com.dsg.wardstudy.common.adapter.MailMessageGenerator;
-import com.dsg.wardstudy.common.adapter.MailSendService;
+import com.dsg.wardstudy.common.adapter.mail.MailMessageGenerator;
+import com.dsg.wardstudy.common.adapter.mail.MailSendService;
+import com.dsg.wardstudy.common.exception.ErrorCode;
+import com.dsg.wardstudy.common.exception.WSApiException;
 import com.dsg.wardstudy.domain.reservation.Reservation;
-import com.dsg.wardstudy.domain.reservation.service.ReservationService;
+import com.dsg.wardstudy.repository.user.UserGroupRepository;
 import com.dsg.wardstudy.type.UserType;
 import lombok.RequiredArgsConstructor;
 import org.slf4j.Logger;
@@ -11,13 +13,10 @@ import org.slf4j.LoggerFactory;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
 
-import java.util.stream.Collectors;
-
 @Service
 @RequiredArgsConstructor
 public class JsonKafkaConsumer {
-
-    private final ReservationService reservationService;
+    private final UserGroupRepository userGroupRepository;
     private final MailMessageGenerator messageGenerator;
     private final MailSendService mailSendService;
     private static final Logger LOGGER = LoggerFactory.getLogger(JsonKafkaConsumer.class);
@@ -25,22 +24,20 @@ public class JsonKafkaConsumer {
     @KafkaListener(topics = "${spring.kafka.topic-json.name}", groupId = "${spring.kafka.consumer.group-id}")
     public void consume(Reservation savedReservation) {
         LOGGER.info(String.format("Json message received -> %s", savedReservation.toString()));
-
-/*        savedReservation.getStudyGroup().getUserGroups().stream()
-                .filter(ug -> ug.getUserType().equals(UserType.PARTICIPANT))
-                .map(ug -> {
-                    if (ug.getUser().getEmail().isEmpty()) {
-                        String toMessage = messageGenerator.toMessage(ug.getUser().getName(), ug.getUser().getReservations());
+        // 비동기 통신 구현 -> 향후 Reservation 도메인에서 다른 프로젝트 서비스단에서 비동기적으로 이메일 알람 구현 가능케 해야
+        // for Reservation 도메인에서 트래픽 부하를 저하
+        Long sgId = savedReservation.getStudyGroup().getId();
+        userGroupRepository.findUserBySGId(sgId).forEach(
+                user -> {
+                    UserType userType = userGroupRepository.findUserTypeByUserIdAndSGId(user.getId(), sgId)
+                            .orElseThrow(() -> new WSApiException(ErrorCode.NO_FOUND_ENTITY));
+                    if ( UserType.PARTICIPANT.equals(userType) && !user.getEmail().isEmpty()) {
+                        String toMessage = messageGenerator.toKafkaMessage(user.getName(), savedReservation);
                         LOGGER.info("sendMail: {}", toMessage);
-                        if (mailSendService.sendMail(ug.getUser().getEmail(), "ward-study 예약룸 알림", toMessage)) {
-                            for (Reservation r : ug.getUser().getReservations()) {
-                                reservationService.changeIsEmailSent(r);
-                            }
-                        }
+                        mailSendService.sendMail(user.getEmail(), "ward-study 예약룸 알림", toMessage);
                     }
-
-                    return null;
-                }).collect(Collectors.toList());*/
+                }
+        );
 
     }
 }
--- src/main/java/com/dsg/wardstudy/common/adapter/mail/MailSendService.java
+++ src/main/java/com/dsg/wardstudy/common/adapter/mail/MailSendService.java
@@ -1,4 +1,4 @@
-package com.dsg.wardstudy.common.adapter;
+package com.dsg.wardstudy.common.adapter.mail;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
