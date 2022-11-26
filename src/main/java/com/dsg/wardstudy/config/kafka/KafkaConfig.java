ccc2909f607760f0d634dcaf5c9d0f2628bdb670
seonggon do <62453668+mooh2jj@users.noreply.github.com>
2022-11-26 22:54:06 +0900

    [#57] kafka 설정 제거

--- src/main/java/com/dsg/wardstudy/config/kafka/KafkaConfig.java
+++ src/main/java/com/dsg/wardstudy/config/kafka/KafkaConfig.java
@@ -6,7 +6,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.config.TopicBuilder;
 
-@Configuration
+/*@Configuration*/
 public class KafkaConfig {
     @Value("${spring.kafka.topic.name}")
     private String topicName;
