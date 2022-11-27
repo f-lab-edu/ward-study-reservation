cff140c3c655e1569d03621e0b318b65f11bf7bd
seonggon do <62453668+mooh2jj@users.noreply.github.com>
2022-11-28 01:23:47 +0900

    [#57] StudyGroup 나머지 api 인증검증로직 추가

--- src/main/java/com/dsg/wardstudy/domain/studyGroup/controller/StudyGroupController.java
+++ src/main/java/com/dsg/wardstudy/domain/studyGroup/controller/StudyGroupController.java
@@ -36,13 +36,17 @@ public class StudyGroupController {
 
     // 스터디그룹 상세보기
     @GetMapping("/study-group/{studyGroupId}")
-    public ResponseEntity<StudyGroupResponse> getById(@PathVariable("studyGroupId") Long studyGroupId) {
+    public ResponseEntity<StudyGroupResponse> getById(
+            @PathVariable("studyGroupId") Long studyGroupId,
+            @AuthUser Long userId
+    ) {
         log.info("studyGroup getById, studyGroupId: {}", studyGroupId);
+        log.info("studyGroup getById, userId: {}", userId);
         return ResponseEntity.ok(studyGroupService.getById(studyGroupId));
     }
 
-    // 스터디그룹 전체조회
-    @GetMapping("/study-group")
+    // 스터디그룹 전체조회 paging 조건에 따른
+    @GetMapping("/study-group/page")
     public ResponseEntity<PageResponse.StudyGroupDetail> getAllPage(
             @PageableDefault(page = 0, size = 10, sort = "id", direction = Sort.Direction.DESC) Pageable pageable,
             @RequestParam(value = "type", required = false) String type,
@@ -53,29 +57,31 @@ public class StudyGroupController {
     }
 
     // 사용자가 참여한 스터디그룹 조회
-    @GetMapping("/users/{userId}/study-group")
+    @GetMapping("/study-group")
     public ResponseEntity<List<StudyGroupResponse>> getAllByUserId(
-            @PathVariable("userId") Long userId
+            @AuthUser Long userId
     ) {
         log.info("studyGroup getAllByUserId, userId: {}", userId);
         return ResponseEntity.ok(studyGroupService.getAllByUserId(userId));
     }
 
     // 스터디그룹 수정(리더만)
-    @PutMapping("/users/{userId}/study-group/{studyGroupId}")
+    @PutMapping("/study-group/{studyGroupId}")
     public Long updateById(
-            @PathVariable("userId") Long userId,
             @PathVariable("studyGroupId") Long studyGroupId,
-            @RequestBody StudyGroupRequest studyGroupRequest) {
+            @RequestBody StudyGroupRequest studyGroupRequest,
+            @AuthUser Long userId
+    ) {
         log.info("studyGroup updateById, userId: {}, studyGroupId: {}, ", userId, studyGroupId);
         return studyGroupService.updateById(userId, studyGroupId, studyGroupRequest);
     }
 
     // 스터디그룹 삭제(리더만)
-    @DeleteMapping("/users/{userId}/study-group/{studyGroupId}")
+    @DeleteMapping("/study-group/{studyGroupId}")
     public ResponseEntity<String> deleteById(
-            @PathVariable("userId") Long userId,
-            @PathVariable("studyGroupId") Long studyGroupId) {
+            @PathVariable("studyGroupId") Long studyGroupId,
+            @AuthUser Long userId
+    ) {
         log.info("studyGroup deleteById, userId: {}, studyGroupId: {}", userId, studyGroupId);
         studyGroupService.deleteById(userId, studyGroupId);
         return new ResponseEntity<>("a study-group successfully deleted!", HttpStatus.OK);
